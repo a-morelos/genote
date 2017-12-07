e21e56b9859d273047dba85ad8347b02b53417de
Alberto Morelos <morelos.alberto@openmailbox.org>
2017-12-07 14:42:02 -0600

    funcion crearnota agregada

--- genotes.py
+++ genotes.py
@@ -10,20 +10,34 @@ group.add_argument("-f", type=int, help="inicio de rango", default=0)
 group.add_argument("-t", type=int, help="fin de rango", default=0)
 
 parser.add_argument("n", type=int, help="numero de notas a generar")
-#parser.add_argument("c", help="Lugar de origen de la referencia")
+#parser.add_argument("-s", help="Lugar de origen de la referencia")
 args = parser.parse_args()
 num = args.n
-#cap = args.c
+#cap = args.s
+
 
 doc, tag, text = Doc().tagtext()
 
-#def generarNota(num):
-#	if (args.f == 0 | args.t == 0):
-#		while(i <= num):
-with tag('div', klass="nota"):
-	with tag('p'):
-		text(num, '. Aquí va el texto de nota.')
-		with tag('a', href="../Text/"):
-			text('&lt&lt')
-nota=doc.getvalue()
-print(nota)
+notas = []
+
+def crearNota(n):
+
+	with tag('div', klass="nota"):
+		with tag('p'):
+			text(n, '. Aquí va el texto de nota.')
+			with tag('a', href="../Text/"):
+				doc.asis('&lt;&lt;')
+	nota=doc.getvalue()
+	return nota
+
+i = 0
+
+while i <= num:
+
+	nueva_nota = crearNota(i + 1)
+	notas.append(nueva_nota)
+
+print('Has creado ', len(notas), 'notas')
+
+for item in notas:
+	print(item)
