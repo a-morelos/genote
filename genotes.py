49f3c67334b0c6923192f132115bc51bfcf31808
Alberto Morelos <morelos.alberto@openmailbox.org>
2017-12-08 15:10:47 -0600

    función crearReferencias agregada

--- genotes.py
+++ genotes.py
@@ -2,14 +2,14 @@
 #Fecha 05/12/2017
 
 import argparse
-from yattag import Doc
+from yattag import Doc, indent
 
 parser = argparse.ArgumentParser(description="Generador de notas y referencias en HTML")
 group = parser.add_argument_group('rango')
 group.add_argument("-f", type=int, help="inicio de rango", default=0)
 group.add_argument("-t", type=int, help="fin de rango", default=0)
 
-parser.add_argument("n", type=int, help="numero de notas a generar")
+parser.add_argument("n", type=int, help="numero de notas a generar, cero para introducir rango")
 #parser.add_argument("-s", help="Lugar de origen de la referencia")
 args = parser.parse_args()
 num = args.n
@@ -18,26 +18,33 @@ num = args.n
 
 doc, tag, text = Doc().tagtext()
 
-notas = []
-
 def crearNota(n):
-
-	with tag('div', klass="nota"):
-		with tag('p'):
+	with tag('div', klass = "nota"):
+		with tag('p', id = n):
 			text(n, '. Aquí va el texto de nota.')
-			with tag('a', href="../Text/"):
+			with tag('a', href = "../Text/"):
 				doc.asis('&lt;&lt;')
-	nota=doc.getvalue()
+	nota = doc.getvalue()
 	return nota
+
+def crearReferencia(n):
+	with tag('sup', id = n):
+		with tag ('a', href = "/Text/notas.xhtml#{}".format(n)):
+			text('[{}]'.format(n))
+	ref = doc.getvalue()
+	return ref
 
 i = 0
 
-while i <= num:
+if num != 0:
+	
+	while i < num:
+		nueva_nota = crearNota(i + 1)
+		nueva_ref = crearReferencia(i + 1)
+		notas = nueva_ref
+		i += 1
+elif args.f == 0 | args.t == 0:
+	print("Debe indicar inicio y fin para rango")
 
-	nueva_nota = crearNota(i + 1)
-	notas.append(nueva_nota)
-
-print('Has creado ', len(notas), 'notas')
-
-for item in notas:
-	print(item)
+print('Has creado ', i, 'notas')
+print (indent(notas))
