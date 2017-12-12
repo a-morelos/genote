66afe80854265e5aea45a1150bdc948e256e6294
Alejandro Solfox <ale.solfox@gmail.com>
2017-12-12 13:35:51 -0600

    Formato de notas listo

--- genotes.py
+++ genotes.py
@@ -9,28 +9,35 @@ group = parser.add_argument_group('rango')
 group.add_argument("-f", type=int, help="inicio de rango", default=0)
 group.add_argument("-t", type=int, help="fin de rango", default=0)
 
-parser.add_argument("n", type=int, help="numero de notas a generar, cero para introducir rango")
+parser.add_argument("n", type=int, help="numero de notas a generar, escriba cero (0) si desea introducir rango")
 #parser.add_argument("-s", help="Lugar de origen de la referencia")
 args = parser.parse_args()
 num = args.n
-#cap = args.s
+ini = args.f
+fin =args.t
+#source = args.s
 
+origen = "capitulo1.xhtml"
+archivo_notas = "notas.xhtml"
 
-doc, tag, text = Doc().tagtext()
+doc, tag, text, line = Doc().ttl()
 
-def crearNota(n):
+def crearNota(n, archivo):
 	with tag('div', klass = "nota"):
-		with tag('p', id = n):
-			text(n, '. Aquí va el texto de nota.')
-			with tag('a', href = "../Text/"):
+		with tag('p', id = "nt{}".format(n)):
+			line('sup', '[{}]'.format(n))
+			text('Aquí va el texto de nota.')
+			with tag('a', href = "../Text/{}#{}".format(archivo, n)):
 				doc.asis('&lt;&lt;')
+	
 	nota = doc.getvalue()
 	return nota
 
-def crearReferencia(n):
+def crearReferencia(n, archivo_notas):
 	with tag('sup', id = n):
-		with tag ('a', href = "/Text/notas.xhtml#{}".format(n)):
-			text('[{}]'.format(n))
+		with tag ('a', href = "../Text/notas.xhtml#nt{}".format(n), id="rf{}".format(n)):
+			line('sup', '[{}]'.format(n))
+	
 	ref = doc.getvalue()
 	return ref
 
@@ -39,12 +46,22 @@ i = 0
 if num != 0:
 	
 	while i < num:
-		nueva_nota = crearNota(i + 1)
-		nueva_ref = crearReferencia(i + 1)
+		nueva_nota = crearNota(i + 1, origen)
+		nueva_ref = crearReferencia(i + 1, archivo_notas)
 		notas = nueva_ref
 		i += 1
+
+	print (indent(notas))
 elif args.f == 0 | args.t == 0:
 	print("Debe indicar inicio y fin para rango")
+else:
+
+	while ini <= fin:
+		nueva_nota = crearNota(ini, origen)
+		nueva_ref = crearReferencia(ini, archivo_notas)
+		notas = nueva_ref
+		ini += 1
+		i+= 1
+	print (indent(notas))
 
 print('Has creado ', i, 'notas')
-print (indent(notas))
